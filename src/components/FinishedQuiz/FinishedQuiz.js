43d0f825aec0ec3b51a52433ad3aa38ff0b88342
DarkenF <69306207+DarkenF@users.noreply.github.com>
2020-08-12 17:52:54 +0300

    Done menu with working links

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,6 +1,7 @@
 import React from "react";
 import classes from './FinishedQuiz.module.css'
 import Button from "../UI/Button/Button";
+import {Link} from 'react-router-dom'
 
 
 const FinishQuiz = props => {
@@ -43,7 +44,9 @@ const FinishQuiz = props => {
 
             <div>
                 <Button onClick={props.onRetry} type='primary'>Повторить</Button>
-                <Button type='success'>Перейти в список тестов</Button>
+                <Link to="/">
+                    <Button type='success'>Перейти в список тестов</Button>
+                </Link>
             </div>
         </div>
     )
